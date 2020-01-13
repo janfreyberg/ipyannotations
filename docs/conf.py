a929f0ebd801f020cffae73d9b9c36d94b4de092
Jan Freyberg <jan.freyberg@gmail.com>
2020-01-13 14:19:56 +0000

    Add initial docs

---
+++ docs/conf.py
@@ -0,0 +1,67 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+import pkg_resources
+
+
+# -- Project information -----------------------------------------------------
+
+project = "ipyannotations"
+copyright = "2020, Jan Freyberg"
+author = "Jan Freyberg"
+
+# The full version, including alpha/beta/rc tags
+version = pkg_resources.get_distribution("ipyannotations").version
+release = version
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = ["m2r", "nbsphinx", "m2r", "numpydoc", "jupyter_sphinx.execute"]
+
+source_suffix = [".rst", ".md"]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "sphinx_rtd_theme"
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+# html_static_path = ["_static"]
+
+nbsphinx_prolog = """
+.. note::
+    It's unfortunately not possible to include the widgets on this website, but
+    you can run an online, live version of this page here:
+    {{ env.doc2path(env.docname, base=None) }}
+"""
+
+nbsphinx_widgets_path = ""
---
+++ docs/index.md
@@ -0,0 +1,20 @@
+# `ipyannotations`: create rich annotations in jupyter notebooks.
+
+The `ipyannotations` library is designed to let you create rich annotations
+for your data (currently, primarily images) inside jupyter notebooks. It lets
+you leverage the rich jupyter display system from python. It was designed to
+integrate with `superintendent`, but does not need to.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   quick-start
+
+
+
+## Indices and tables
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
