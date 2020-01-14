b621220261a7803bc59185da7202f1643be121fd
Jan Freyberg <jan.freyberg@gmail.com>
2020-01-14 11:34:31 +0000

    Update undo functions for polygon

--- src/ipyannotations/images/canvases/polygon.py
+++ src/ipyannotations/images/canvases/polygon.py
@@ -17,8 +17,6 @@ class Polygon:
     label: Optional[str] = None
     close_threshold: int = 5
 
-    closed = False
-
     def append(self, point: Tuple[int, int]):
         # if self.closed:
         #     raise ValueError("Can't append to a closed polygon.")
@@ -26,15 +24,19 @@ class Polygon:
         point = (round(point[0]), round(point[1]))
         self.points.append(point)
         if self._is_closed():
+            # ensure last point is identical to first:
             self.points.pop(-1)
             self.points.append(self.points[0])
-            self.closed = True
 
     @property
     def xy_lists(self):
         if len(self.points) == 0:
             return [], []
         return map(list, zip(*self.points))
+
+    @property
+    def closed(self) -> bool:
+        return len(self.points) > 2 and self.points[0] == self.points[-1]
 
     def _is_closed(self) -> bool:
 
@@ -127,7 +129,7 @@ class PolygonAnnotationCanvas(AbstractAnnotationCanvas):
     @trigger_redraw
     def _undo_new_polygon(self):
         self.current_polygon = self.polygons.pop(-1)
-        self.current_polygon.pop(-1)
+        self.current_polygon.points.pop(-1)
 
     def re_draw(self):
 
