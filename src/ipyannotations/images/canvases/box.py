85138f59014f2b1718fe7a77d3d073066227fdce
thomassajot <thomas.sajot@blueprism.com>
2020-06-01 09:36:51 +0100

    mypy typing

--- src/ipyannotations/images/canvases/box.py
+++ src/ipyannotations/images/canvases/box.py
@@ -38,9 +38,9 @@ class BoundingBoxAnnotationCanvas(AbstractAnnotationCanvas):
     def draw_box(self, box: BoundingBox, proposed: bool = False):
         box = copy.deepcopy(box)
         box.xyxy = (  # type: ignore
-            *self.map_image_coords_to_canvas(*box.xyxy[:2]),
-            *self.map_image_coords_to_canvas(*box.xyxy[2:]),
-        )
+            *self.map_image_coords_to_canvas(*box.xyxy[:2]),  # type: ignore
+            *self.map_image_coords_to_canvas(*box.xyxy[2:]),  # type: ignore
+        )  # type: ignore
 
         color = self.colormap.get(box.label, "#000000")
         canvas = self[1]
