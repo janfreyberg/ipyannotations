e56c7b5d957b4857167a9b82d57d6850dc559374
Jan Freyberg <jan.freyberg@gmail.com>
2021-02-05 11:12:46 +0000

    Add loading lab/nb extensions to init of package

--- src/ipyannotations/__init__.py
+++ src/ipyannotations/__init__.py
@@ -1,6 +1,46 @@
-"""Annotate data in jupyter notebooks."""
-__version__ = "0.2.1"
+"""Create rich adata annotations in jupyter notebooks."""
 
-from .images import PolygonAnnotator, PointAnnotator, BoxAnnotator
+from ._version import __version__, version_info
 
-__all__ = ["PolygonAnnotator", "PointAnnotator", "BoxAnnotator"]
+def _jupyter_labextension_paths():
+    """Called by Jupyter Lab Server to detect if it is a valid labextension and
+    to install the widget
+
+    Returns
+    =======
+    src: Source directory name to copy files from. Webpack outputs generated files
+        into this directory and Jupyter Lab copies from this directory during
+        widget installation
+    dest: Destination directory name to install widget files to. Jupyter Lab copies
+        from `src` directory into <jupyter path>/labextensions/<dest> directory
+        during widget installation
+    """
+    return [{
+        'src': 'labextension',
+        'dest': 'ipyannotations',
+    }]
+
+
+def _jupyter_nbextension_paths():
+    """Called by Jupyter Notebook Server to detect if it is a valid nbextension and
+    to install the widget
+
+    Returns
+    =======
+    section: The section of the Jupyter Notebook Server to change.
+        Must be 'notebook' for widget extensions
+    src: Source directory name to copy files from. Webpack outputs generated files
+        into this directory and Jupyter Notebook copies from this directory during
+        widget installation
+    dest: Destination directory name to install widget files to. Jupyter Notebook copies
+        from `src` directory into <jupyter path>/nbextensions/<dest> directory
+        during widget installation
+    require: Path to importable AMD Javascript module inside the
+        <jupyter path>/nbextensions/<dest> directory
+    """
+    return [{
+        'section': 'notebook',
+        'src': 'nbextension',
+        'dest': 'ipyannotations',
+        'require': 'ipyannotations/extension'
+    }]
