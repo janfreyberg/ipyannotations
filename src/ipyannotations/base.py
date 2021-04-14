fe38b8734025e04a73f153d949fc82337f08e8df
Jan Freyberg <jan.freyberg@gmail.com>
2021-04-14 07:47:38 +0100

    Add clear method to base widget

--- src/ipyannotations/base.py
+++ src/ipyannotations/base.py
@@ -1,5 +1,5 @@
 """The base class that data labelling widgets should inherit from."""
-from typing import Any, Callable, List, Optional
+from typing import Callable, List, Optional
 
 import ipyevents
 import ipywidgets as widgets
@@ -8,7 +8,7 @@ CallbackList = List[Callable]
 
 
 class LabellingWidgetMixin:
-    def __init__(self, *args, track_keystrokes=True, **kwargs):
+    def __init__(self, *args, track_keystrokes=True, **kwargs):  # noqa: D001
         super().__init__(*args, **kwargs)
         button_layout = widgets.Layout(
             # width="auto",
@@ -72,6 +72,11 @@ class LabellingWidgetMixin:
 
         This is called by the button / text field elements and shouldn't be
         called directly.
+
+        Parameters
+        ----------
+        sender : Optional
+            The "sender" that invoked this callback. This is ignored.
         """
         if hasattr(self, "data"):
             value = self.data
@@ -94,6 +99,14 @@ class LabellingWidgetMixin:
         self.undo_fns.append(callback)
 
     def undo(self, sender=None):
+        """
+        Undo (i.e. call the functions in the undo queue.)
+
+        Parameters
+        ----------
+        sender : Optional
+            The "sender" that invoked this callback. This is ignored.
+        """
         if self._undo_queue:
             last_undo_fn = self._undo_queue.pop()
             last_undo_fn()
@@ -102,8 +115,21 @@ class LabellingWidgetMixin:
                 callback()
 
     def skip(self, sender: Optional[widgets.Button] = None):
+        """
+        Skip a data point.
+
+        Parameters
+        ----------
+        sender : Optional
+            The "sender" that invoked this callback. This is ignored.
+        """
         for callback in self.submission_functions:
             callback(None)
+
+    def clear(self):
+        """Clear this widget's data."""
+        if "data" in self.class_traits():
+            self.data = self.class_traits()["data"].default()
 
     def _handle_keystroke(self, event):
         if event["type"] != "keyup":
