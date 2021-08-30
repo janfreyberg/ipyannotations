4c784755ca02a82f34f9d847dc728a09a52dcad3
Jan Freyberg <jan.freyberg@gmail.com>
2021-08-30 21:19:13 +0100

    Small bugfix D:

--- ipyannotations/_doc_utils.py
+++ ipyannotations/_doc_utils.py
@@ -27,7 +27,6 @@ def recursively_remove_from_dom(
 
 
 def patch_canvas(annotator, img_path):
-    img_path = os.path.join("docs", img_path)
     with open(img_path, "rb") as f:
         bytesval = f.read()
     img_widg = widgets.Image(value=bytesval)
