476d6b18a491ddf92852021240c00bc239768a59
Jan Freyberg <jan.freyberg@gmail.com>
2021-09-03 13:20:53 +0100

    Update versions of python and npm packages

--- ipyannotations/_version.py
+++ ipyannotations/_version.py
@@ -4,5 +4,5 @@
 # Copyright (c) Jan Freyberg.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 3, 0, "dev1")
+version_info = (0, 4, 0)
 __version__ = ".".join(map(str, version_info))
